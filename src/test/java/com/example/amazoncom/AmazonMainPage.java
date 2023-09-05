5e47ea18cc784170219ce777d1b13a767dbb4140
“Julio <Julio_Morales@epam.com>
2023-09-05 17:23:05 +0200

    Init project

---
+++ src/test/java/com/example/amazoncom/AmazonMainPage.java
@@ -0,0 +1,42 @@
+package com.example.amazoncom;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AmazonMainPage {
+
+  final static String URL_WEB_AMAZON = "https://www.amazon.es/";
+  WebDriver driver;
+  @FindBy(id = "twotabsearchtextbox")
+  WebElement searchField;
+  @FindBy(id = "nav-search-submit-button")
+  WebElement searchButton;
+  @FindBy(id = "nav-cart")
+  WebElement linkCart;
+  @FindBy(css = "a[class='a-text-center'][2]")
+  WebElement linkDaysOffer;
+  @FindBy(css = "div[class=\"a-section maple-banner__text\"]")
+  WebElement textAmazonRecargas;
+  @FindBy(css = "h2")
+  WebElement emptyCartMessage;
+
+  public AmazonMainPage(WebDriver driver) {
+    this.driver = driver;
+    PageFactory.initElements(driver, this);
+  }
+
+  public void openHomePage() {
+    driver.get(URL_WEB_AMAZON);
+    WebElement waiting = (new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+        ExpectedConditions.visibilityOf(searchField)));
+  }
+
+  public String getSearchFieldValue() {
+    return searchField.getAttribute("value");
+  }
+}
---
+++ src/test/java/com/example/amazoncom/AmazonTest.java
@@ -0,0 +1,76 @@
+package com.example.amazoncom;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AmazonTest {
+
+  public WebDriver driver;
+  AmazonMainPage mainPage;
+
+
+  @Given("Obtain the content a website: amazon.es")
+  @BeforeAll
+  public void visitAmazon() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("incognito");
+    driver = new ChromeDriver(chromeOptions);
+    mainPage = new AmazonMainPage(driver);
+    mainPage.openHomePage();
+  }
+
+  @When("When it searches 'selenium' and checks the assertion of 'Selenium' in the search box")
+  @Test
+  @Order(1)
+  public void searchAndCheckTheAssersion() {
+    // 1 assertion
+    mainPage.searchField.sendKeys("Selenium");
+    mainPage.searchButton.click();
+    Assertions.assertThat(mainPage.getSearchFieldValue())
+        .as("Message Error: Word selenium not found").isEqualTo("Selenium");
+
+  }
+
+  @And("Click link to cart and Check the assertion 'Tu cesta de Amazon est")
+  @Test
+  @Order(3)
+  public void visitLinkOfferAndCheckTitle() {
+    // 3 assertion
+    mainPage.linkCart.click();
+    Assertions.assertThat(mainPage.emptyCartMessage.getText())
+        .as("Failure message: Offer doesn't appear").containsIgnoringCase("Tu cesta de Amazon est");
+
+  }
+
+  @And("Click link to cart and check the assertion of 'con Amazon Recargas'")
+  @Test
+  @Order(2)
+  public void clickToCartAndCheckOffers() {
+    // 2 assertion
+    mainPage.linkCart.click();
+    Assertions.assertThat(mainPage.textAmazonRecargas.getText())
+        .as("Failure message: Today Offer doesn't appear")
+        .containsIgnoringCase("con Amazon Recargas");
+
+  }
+
+  @Then("Then It will close all browser drivers which have been opened")
+  @AfterAll
+  public void teardown() {
+    driver.quit();
+  }
+}
