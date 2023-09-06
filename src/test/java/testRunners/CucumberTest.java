f9cb7a39df1db9ed59c6d56749cdc323eb1532dd
“Julio <Julio_Morales@epam.com>
2023-09-06 13:52:18 +0200

    hw4 trying to configure files

--- src/test/java/testRunners/CucumberTest.java
+++ src/test/java/testRunners/CucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources/AmazonTest.feature", glue = "src/test/java/stepsDefs/HomePageSteps.java")
+@CucumberOptions(features = "src/test/resources/AmazonTest.feature", glue = "stepsDefs.HomePageSteps.java")
 public class CucumberTest {
 
 }
